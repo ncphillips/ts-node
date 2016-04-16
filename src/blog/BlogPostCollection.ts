22b7be5b5c5dcbf823cf23c4b7209e368ba39066
ncphillips <ncphillips@upei.ca>
2016-04-16 14:39:43 -0300

    Added a `findAll` method to BlogPostCollection and implemented

--- src/blog/BlogPostCollection.ts
+++ src/blog/BlogPostCollection.ts
@@ -1,5 +1,6 @@
 import BlogPost = require("./BlogPost");
 export interface BlogPostCollection {
-  findById(id: string|number, callback: (err:Error, post:BlogPost)=>any): void;
+  findById(id: number, callback: (err:Error, post:BlogPost)=>any): void;
   save(post: BlogPost, callback: (err:Error, post:BlogPost)=>any): void;
+  findAll(callback: (err:Error, posts:BlogPost[])=>any): void;
 }
--- src/lib/collections/MemoryBlogPostCollection.ts
+++ src/lib/collections/MemoryBlogPostCollection.ts
@@ -1,16 +1,28 @@
 import {BlogPostCollection} from "../../blog/BlogPostCollection";
 import BlogPost = require("../../blog/BlogPost");
-export class MemoryBlogPostCollection implements BlogPostCollection {
-  constructor() {
-    console.log("Creating in memory blog post collection");
+
+var posts: BlogPost[] = [];
+
+class MemoryBlogPostCollection implements BlogPostCollection {
+  findAll(callback:(err:Error, posts:BlogPost[])=>any):void {
+    callback(null, posts);
   }
-  findById(id:string|number, callback:(err:Error, post:BlogPost)=>any): void {
-    console.log("findById");
+  findById(id:number, callback:(err:Error, post:BlogPost)=>any): void {
+    if (!posts.hasOwnProperty(id+"")) {
+      console.log(posts);
+      callback(new Error("Not found"), null);
+    } else {
+      callback(null, posts[id]);
+    }
   }
 
   save(post:BlogPost, callback:(err:Error, post:BlogPost)=>any): void {
-    console.log("save");
+    if(!post.id) {
+      post.id = posts.length;
+    }
+    posts[post.id] = post;
+    callback(null, post);
   }
 }
 
-module.exports = MemoryBlogPostCollection;
+module.exports = new MemoryBlogPostCollection();
