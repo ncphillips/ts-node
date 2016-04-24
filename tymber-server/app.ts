c310e277ebf0ff50bc541182e2bd43dec9650d33
ncphillips <ncphillips@upei.ca>
2016-04-24 16:10:58 -0300

    Server setup is distributed across files now, and the dependency injector is included via npm

--- tymber-server/app.ts
+++ tymber-server/app.ts
@@ -1,67 +1,27 @@
 import {Request} from "express";
 import {Response} from "express";
 import {NextFunction} from "express";
+
 var express = require('express');
-var exphbs  = require('express-handlebars');
-var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./main/index');
-var blog = require('./blog/routes');
 
 var app = express();
-var hbs = exphbs.create();
 
-// view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+require('./setup/viewEngine')(app);
+require('./setup/bodyParsing')(app);
+require('./setup/staticFiles')(app);
+require('./setup/routes')(app);
+require('./setup/depInjection')(app);
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(require("./middleware/404"));
 
-app.use('/', routes);
-app.use('/blog', blog);
-
-// catch 404 and forward to error handler
-app.use(function(req: Request, res: Response, next: NextFunction) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+  app.use(require('./middleware/500dev'));
+} else {
+  app.use(require('./middleware/500'));
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
--- tymber-server/blog/BlogPostCollection.ts
+++
@@ -1,6 +0,0 @@
-import BlogPost = require("./BlogPost");
-export interface BlogPostCollection {
-  findById(id: number, callback: (err:Error, post:BlogPost)=>any): void;
-  save(post: BlogPost, callback: (err:Error, post:BlogPost)=>any): void;
-  findAll(callback: (err:Error, posts:BlogPost[])=>any): void;
-}
